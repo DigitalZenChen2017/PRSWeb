75fc003291ece757b421a34a6c128e2e1dbebc22
DigitalZenChen2017 <31525768+DigitalZenChen2017@users.noreply.github.com>
2019-02-14 12:22:48 -0500

    Updated VendorController class

--- src/main/java/com/prs/web/VendorController.java
+++ src/main/java/com/prs/web/VendorController.java
@@ -6,8 +6,12 @@ import com.prs.business.vendor.VendorRepository;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,4 +47,44 @@ public class VendorController {
 		}
 		return jr;
 	}
+
+	@PostMapping("/")
+	public JsonResponse addVendor(@RequestBody Vendor v) {
+		return saveVendor(v);
+
+	}
+
+	@PutMapping("/{id}")
+	public JsonResponse updateVendor(@RequestBody Vendor v, @PathVariable int id) {
+		return saveVendor(v);
+	}
+
+	private JsonResponse saveVendor(Vendor v) {
+		JsonResponse jr = null;
+		try {
+			vendorRepository.save(v);
+			jr = JsonResponse.getInstance(v);
+		} catch (Exception ex) {
+			jr = JsonResponse.getInstance(ex.getMessage());
+		}
+		return jr;
+	}
+
+	@DeleteMapping("/{id}")
+	public JsonResponse deleteVendor(@PathVariable int id) {
+		JsonResponse jr = null;
+		try {
+			Optional<Vendor> v = vendorRepository.findById(id);
+			if (v.isPresent()) {
+				vendorRepository.deleteById(id);
+				jr = JsonResponse.getInstance(v);
+			} else {
+				jr = JsonResponse.getInstance("No user found for id: " + id);
+			}
+		} catch (Exception ex) {
+			jr = JsonResponse.getInstance(ex.getMessage());
+		}
+		return jr;
+	}
+
 }
